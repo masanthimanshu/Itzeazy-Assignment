4a531d4366eaf8848b9717175742ddf47c750333
Himanshu Masant <masanthimanshu@gmail.com>
2022-02-13 22:59:09 +0530

    Initial Commit

---
+++ app.js
@@ -0,0 +1,43 @@
+import express from "express";
+import { engine } from "express-handlebars";
+import fs from "node:fs";
+
+// Handlebars Objects
+
+const homeObj = JSON.parse(fs.readFileSync("./json/home.json"));
+const twoObj = JSON.parse(fs.readFileSync("./json/page2.json"));
+const threeobj = JSON.parse(fs.readFileSync("./json/page3.json"));
+const fourObj = JSON.parse(fs.readFileSync("./json/page4.json"));
+const finalObj = JSON.parse(fs.readFileSync("./json/final.json"));
+
+const port = process.env.PORT || 3000;
+const app = express();
+
+app.use(express.static("public"));
+
+app.engine("handlebars", engine());
+app.set("view engine", "handlebars");
+
+app.get("/", (req, res) => {
+  res.render("index", homeObj);
+});
+
+app.post("/two", (req, res) => {
+  res.render("page2", twoObj);
+});
+
+app.post("/three", (req, res) => {
+  res.render("page3", threeobj);
+});
+
+app.post("/four", (req, res) => {
+  res.render("page4", fourObj);
+});
+
+app.post("/final", (req, res) => {
+  res.render("final", finalObj);
+});
+
+app.listen(port, () => {
+  console.log(`Active On Port ${port}`);
+});
---
+++ public/js/script.js
@@ -0,0 +1,9 @@
+// Page 2
+$("#place").change(function () {
+  $("#myForm").submit();
+});
+
+// Page 3
+$("#currentDL").change(function () {
+  $("#myForm").submit();
+});
